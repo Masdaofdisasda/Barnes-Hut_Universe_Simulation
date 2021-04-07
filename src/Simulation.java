c7e8af962fa71608a7fbf027982b5ff6b3a37197
David Köppl <david.koeppl75@gmail.com>
2021-04-07 20:52:12 +0200

    Grundlegende Klassen eingeführt

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,7 +1,18 @@
 public class Simulation {
 
+    // gravitational constant
+    public static final double G = 6.6743e-11;
+
     public static void main(String[] args) {
         //TODO: please use this class to run your simulation
+
+        //Create Simulation Data
+        UniverseTree observableUniverse = new UniverseTree;
+
+        int numOfAstronomicalBodies = 10;
+        for (int i = 0; i < 10; i++) {
+
+        }
 
     }
 
